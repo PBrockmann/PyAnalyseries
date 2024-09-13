b583d5408444baa8f5eb130965902b782623c402
PBrockmann <Patrick.Brockmann@lsce.ipsl.fr>
2024-09-13 10:05:51 +0200

    Lineage ok

--- lineage.py
+++ lineage.py
@@ -9,12 +9,40 @@ import pandas as pd
 from matplotlib.patches import ConnectionPatch
 from matplotlib.lines import Line2D
 import matplotlib.patches as patches
+from scipy import interpolate
 
 import matplotlib as mpl
 
 mpl.rcParams['toolbar'] = 'None'
-plt.rc('xtick',labelsize=8)
-plt.rc('ytick',labelsize=8)
+mpl.rcParams['axes.labelsize'] = 10
+mpl.rcParams['xtick.labelsize'] = 8
+mpl.rcParams['ytick.labelsize'] = 8
+
+#=========================================================================================
+key_x = False
+key_shift = False
+key_control = False
+vline1 = None
+vline2 = None
+press = None
+cur_xlim = None
+cur_ylim = None
+xpress = None
+ypress = None
+mousepress = None
+artistsList_Dict = {} 
+vline1List = []
+vline2List = []
+coordsX1 = []
+coordsX2 = []
+curve1 = None
+curve2 = None
+curve2Interp = None
+x1 = None
+y1 = None
+x2 = None
+y2 = None
+x2Interp = None
 
 #=========================================================================================
 def readData(file, x1Name, y1Name, x2Name, y2Name):
@@ -34,46 +62,61 @@ def readData(file, x1Name, y1Name, x2Name, y2Name):
 
 #=========================================================================================
 def readPointers(file):
-    global artistsList_Dict, vline1List, vline2List
+    global coordsX1, coordsX2
 
     try:
         df = pd.read_csv(file, names=['coordsX1','coordsX2'])
         print(df.to_string(index=False, header=False, float_format="%.8f"))
-        for index, row in df.iterrows():
-            coordX1 = row['coordsX1']
-            coordX2 = row['coordsX2']
-            vline1 = axs[0].axvline(coordX1, color='b', linestyle='--', linewidth=1, label='vline')
-            vline2 = axs[1].axvline(coordX2, color='b', linestyle='--', linewidth=1, label='vline')
-            vline1List.append(vline1)
-            vline2List.append(vline2)
-            connect = ConnectionPatch(color='b', picker=5, clip_on=True, label='connection',
-                        xyA=(coordX1, axs[0].get_ylim()[0]), coordsA=axs[0].transData,
-                        xyB=(coordX2, axs[1].get_ylim()[1]), coordsB=axs[1].transData)
-            fig.add_artist(connect)
-            artistsList_Dict[id(connect)] = [connect, vline1, vline2]
-
-        axs[0].autoscale()
-        axs[1].autoscale()
-        updateConnect()
+        coordsX1 = df['coordsX1'].to_numpy()
+        coordsX2 = df['coordsX2'].to_numpy()
 
     except:
         print("Error: reading pointers file")
 
 #=========================================================================================
-key_x = False
-key_shift = False
-key_control = False
-vline1 = None
-vline2 = None
-press = None
-cur_xlim = None
-cur_ylim = None
-xpress = None
-ypress = None
-mousepress = None
-artistsList_Dict = {} 
-vline1List = []
-vline2List = []
+def updateConnections():
+
+    for artistsList in artistsList_Dict.values():
+        if isinstance(artistsList[0], ConnectionPatch):
+            connect = artistsList[0]
+            x1, y1 = connect.xy1
+            connect.xy1 = (x1, axs[0].get_ylim()[0]) 
+            x2, y2 = connect.xy2
+            connect.xy2 = (x2, axs[1].get_ylim()[1])
+            if ((axs[0].get_xlim()[0] < x1 < axs[0].get_xlim()[1]) and
+                (axs[1].get_xlim()[0] < x2 < axs[1].get_xlim()[1])):
+                connect.set_visible(True)
+            else:
+                connect.set_visible(False)
+
+#=========================================================================================
+def drawConnections(interpolate=False):
+    global artistsList_Dict, vline1List, vline2List
+
+    for objectId in artistsList_Dict.keys():
+        for artist in artistsList_Dict[objectId]:
+            artist.remove()
+    artistsList_Dict = {}
+    vline1List = []
+    vline2List = []
+
+    for i in range(len(coordsX1)):
+        coordX1 = coordsX1[i]
+        coordX2 = coordsX2[i]
+        if interpolate: coordX2 = coordX1
+        vline1 = axs[0].axvline(coordX1, color='b', alpha=0.5, linestyle='--', linewidth=1, label='vline')
+        vline2 = axs[1].axvline(coordX2, color='b', alpha=0.5, linestyle='--', linewidth=1, label='vline')
+        vline1List.append(vline1)
+        vline2List.append(vline2)
+        connect = ConnectionPatch(color='b', alpha=0.5, linewidth=1, picker=5, clip_on=True, label='connection',
+                    xyA=(coordX1, axs[0].get_ylim()[0]), coordsA=axs[0].transData,
+                    xyB=(coordX2, axs[1].get_ylim()[1]), coordsB=axs[1].transData)
+        fig.add_artist(connect)
+        artistsList_Dict[id(connect)] = [connect, vline1, vline2]
+
+    axs[0].autoscale()
+    axs[1].autoscale()
+    updateConnections()
 
 #=========================================================================================
 def onPick(event):
@@ -82,6 +125,7 @@ def onPick(event):
     artistLabel = event.artist.get_label()
     #print(artistLabel)
 
+    #-----------------------------------------------
     if artistLabel == 'connection':
         if key_x:
             objectId = id(event.artist)
@@ -94,6 +138,7 @@ def onPick(event):
             del artistsList_Dict[objectId]
             plt.draw()
 
+    #-----------------------------------------------
     elif artistLabel == 'curve':
         if key_shift:
             coordPoint = [event.mouseevent.xdata, event.mouseevent.ydata]
@@ -101,14 +146,15 @@ def onPick(event):
                 if vline1 != None:
                     vline1.set_data([coordPoint[0], coordPoint[0]], [0,1])
                 else:
-                    vline1 = axs[0].axvline(coordPoint[0], color='b', linestyle='--', linewidth=1, label='vline')
+                    vline1 = axs[0].axvline(coordPoint[0], color='b', alpha=0.5, linestyle='--', linewidth=1, label='vline')
             elif event.artist == curve2:
                 if vline2 != None:
                     vline2.set_data([coordPoint[0], coordPoint[0]], [0,1])
                 else:
-                    vline2 = axs[1].axvline(coordPoint[0], color='b', linestyle='--', linewidth=1, label='vline')
+                    vline2 = axs[1].axvline(coordPoint[0], color='b', alpha=0.5, linestyle='--', linewidth=1, label='vline')
             plt.draw()
 
+    #-----------------------------------------------
     elif artistLabel == 'points':
         if key_control:
             ind = event.ind[0]
@@ -127,55 +173,66 @@ def onPick(event):
             plt.draw()
 
 #=========================================================================================
-def updateConnect():
-    for artistsList in artistsList_Dict.values():
-        if isinstance(artistsList[0], ConnectionPatch):
-            connect = artistsList[0]
-            x1, y1 = connect.xy1
-            connect.xy1 = (x1, axs[0].get_ylim()[0]) 
-            x2, y2 = connect.xy2
-            connect.xy2 = (x2, axs[1].get_ylim()[1]) 
-
-#=========================================================================================
 def zoom(event):
-    scale_zoom = 1.2
-    cur_xlim = event.inaxes.get_xlim()
-    cur_ylim = event.inaxes.get_ylim()
-    xdata = event.xdata # get event x location
-    ydata = event.ydata # get event y location
-    if event.button == 'up':                        # zoom in
-        scale_factor = 1 / scale_zoom
-    elif event.button == 'down':                    # zoom out
-        scale_factor = scale_zoom
-    else:
-        # deal with something that should never happen
-        scale_factor = 1
-    new_width = (cur_xlim[1] - cur_xlim[0]) * scale_factor
-    new_height = (cur_ylim[1] - cur_ylim[0]) * scale_factor
-    relx = (cur_xlim[1] - xdata)/(cur_xlim[1] - cur_xlim[0])
-    rely = (cur_ylim[1] - ydata)/(cur_ylim[1] - cur_ylim[0])
     
-    event.inaxes.set_xlim([xdata - new_width * (1-relx), xdata + new_width * (relx)])
-    event.inaxes.set_ylim([ydata - new_height * (1-rely), ydata + new_height * (rely)])
-    
-    updateConnect()
-    event.inaxes.figure.canvas.draw()
+    if event.inaxes in axs:
+        scale_zoom = 1.2
+        cur_xlim = event.inaxes.get_xlim()
+        cur_ylim = event.inaxes.get_ylim()
+        xdata = event.xdata # get event x location
+        ydata = event.ydata # get event y location
+        if event.button == 'up':                        # zoom in
+            scale_factor = 1 / scale_zoom
+        elif event.button == 'down':                    # zoom out
+            scale_factor = scale_zoom
+        else:
+            # deal with something that should never happen
+            scale_factor = 1
+        new_width = (cur_xlim[1] - cur_xlim[0]) * scale_factor
+        new_height = (cur_ylim[1] - cur_ylim[0]) * scale_factor
+        relx = (cur_xlim[1] - xdata)/(cur_xlim[1] - cur_xlim[0])
+        rely = (cur_ylim[1] - ydata)/(cur_ylim[1] - cur_ylim[0])
+        
+        event.inaxes.set_xlim(xdata - new_width * (1-relx), xdata + new_width * (relx))
+        event.inaxes.set_ylim(ydata - new_height * (1-rely), ydata + new_height * (rely))
+        
+        updateConnections()
+        event.inaxes.figure.canvas.draw()
     
 #=========================================================================================
 def onKeyPress(event):
-    global key_x, key_shift, key_control, vline1, vline2
+    global key_x, key_shift, key_control, vline1, vline2, x2Interp, curve2Interp
 
     sys.stdout.flush()
 
+    #-----------------------------------------------
     if event.key == 'a':
-        event.inaxes.relim()
-        event.inaxes.autoscale()
-        updateConnect()
-        event.inaxes.figure.canvas.draw()
+        linecursor1.set_visible(False)
+        linecursor2.set_visible(False)
+        curve1.set_visible(False)
+        curve2.set_visible(False)
+        #if curve2Interp: curve2Interp.set_visible(False)
+        axs[0].relim(visible_only=True)
+        axs[1].relim(visible_only=True)
+        #axsInterp.relim(visible_only=True)
+        axs[0].autoscale()
+        axs[1].autoscale()
 
+        curve1.set_visible(True)
+        curve2.set_visible(True)
+        axs[0].relim()
+        axs[0].autoscale(axis='y')
+        axs[1].relim()
+        axs[1].autoscale(axis='y')
+
+        updateConnections()
+        plt.draw()
+
+    #-----------------------------------------------
     if event.key == 'x':
         key_x = True
 
+    #-----------------------------------------------
     if event.key == 'c':
         if vline1 != None and vline2 != None :
             coordX1 = float(vline1.get_xdata()[0])
@@ -186,7 +243,7 @@ def onKeyPress(event):
             if np.searchsorted(coordsX1, coordX1) != np.searchsorted(coordsX2, coordX2):
                 print("Error: Connection not possible because it would cross existing connections") 
                 return
-            connect = ConnectionPatch(color='b', picker=5, clip_on=True, label='connection',
+            connect = ConnectionPatch(color='b', alpha=0.5, linewidth=1, picker=5, clip_on=True, label='connection',
                         xyA=(coordX1, axs[0].get_ylim()[0]), coordsA=axs[0].transData,
                         xyB=(coordX2, axs[1].get_ylim()[1]), coordsB=axs[1].transData)
             fig.add_artist(connect)
@@ -197,16 +254,42 @@ def onKeyPress(event):
             vline2 = None
             plt.draw()
 
+    #-----------------------------------------------
+    elif event.key == 's':
+        df = pd.DataFrame({x1Name: x1, y1Name: y1, x2Name: x2, y2Name: y2, 
+                           y2Name + ' interpolated on ' + x1Name: x2Interp})
+        print("Info: saved data in file data.csv")
+        df.to_csv('data.csv', index=False, float_format="%.8f")
+
+    #-----------------------------------------------
     elif event.key == 'i':
-        coordsX1 = [float(line.get_xdata()[0]) for line in vline1List]
-        coordsX2 = [float(line.get_xdata()[0]) for line in vline2List]
+        coordsX1 = sorted([float(line.get_xdata()[0]) for line in vline1List])
+        coordsX2 = sorted([float(line.get_xdata()[0]) for line in vline2List])
         df = pd.DataFrame({'coordsX1': coordsX1, 'coordsX2': coordsX2})
+        print("Info: saved pointers in file pointers.csv")
         df.to_csv('pointers.csv', index=False, header=False, float_format="%.8f")
         print(df.to_string(index=False, header=False, float_format="%.8f"))
 
+    #-----------------------------------------------
+    elif event.key == 'z':
+        if len(vline1List) <= 1:
+            print("Warning: interpolation needs a minimum of 2 pointers")
+            return 
+        cur_xlim = axs[0].get_xlim()
+        axsInterp.set_visible(True)
+        coordsX1 = sorted([float(line.get_xdata()[0]) for line in vline1List])
+        coordsX2 = sorted([float(line.get_xdata()[0]) for line in vline2List])
+        f = interpolate.interp1d(coordsX2, coordsX1, fill_value="extrapolate")
+        x2Interp = f(x2)
+        curve2Interp, = axsInterp.plot(x2Interp, y2, color=curve2Color, linewidth=0.5)
+        axsInterp.set_xlim(cur_xlim)
+        plt.draw()
+
+    #-----------------------------------------------
     elif event.key == 'shift':
         key_shift = True
 
+    #-----------------------------------------------
     elif event.key == 'control':
         key_control = True
         if event.inaxes == axs[0]:
@@ -220,12 +303,25 @@ def onKeyRelease(event):
     global key_x, key_shift, key_control
 
     sys.stdout.flush()
+
+    #-----------------------------------------------
     if event.key == 'x':
         key_x = False
 
+    #-----------------------------------------------
+    elif event.key == 'z':
+        if curve2Interp:
+            curve2Interp.remove()
+            x2Interp = []
+            axsInterp.set_visible(False)
+            axsInterp.clear()
+            plt.draw()
+
+    #-----------------------------------------------
     elif event.key == 'shift':
         key_shift = False 
 
+    #-----------------------------------------------
     elif event.key == 'control':
         key_control = False 
         if event.inaxes == axs[0]:
@@ -240,10 +336,14 @@ def onPress(event):
 
     if event.inaxes not in axs: return
 
+    #-----------------------------------------------
     if event.button == 3:
-        mousepress = "right"
+        mousepress = 'right'
+
+    #-----------------------------------------------
     elif event.button == 1:
-        mousepress = "left"
+        mousepress = 'left'
+
     cur_xlim = event.inaxes.get_xlim()
     cur_ylim = event.inaxes.get_ylim()
     press = event.xdata, event.ydata
@@ -258,6 +358,7 @@ def onRelease(event):
 def onMotion(event):
     global cur_xlim, cur_ylim, press
 
+    #-----------------------------------------------
     if event.inaxes not in axs:
         press = None
         linecursor1.set_visible(False)
@@ -265,6 +366,7 @@ def onMotion(event):
         plt.draw()
         return
 
+    #-----------------------------------------------
     if event.inaxes is axs[0]:
         linecursor1.set_visible(True)
         linecursor2.set_visible(False)
@@ -275,27 +377,33 @@ def onMotion(event):
         linecursor2.set_xdata([event.xdata])
     event.inaxes.figure.canvas.draw()
 
+    #-----------------------------------------------
     if press is None: return
 
-    if mousepress == "left":
+    #-----------------------------------------------
+    if mousepress == 'left':
         linecursor1.set_visible(False)
         linecursor2.set_visible(False)
         dx = event.xdata - xpress
         dy = event.ydata - ypress
         cur_xlim -= dx
         cur_ylim -= dy
-        event.inaxes.set_xlim(cur_xlim)
-        event.inaxes.set_ylim(cur_ylim)
-    elif mousepress == "right":
+        event.inaxes.set_xlim(cur_xlim[0], cur_xlim[1])
+        event.inaxes.set_ylim(cur_ylim[0], cur_ylim[1])
+
+    #-----------------------------------------------
+    elif mousepress == 'right':
         linecursor1.set_visible(False)
         linecursor2.set_visible(False)
         dx = event.xdata - xpress
         dy = event.ydata - ypress
-        event.inaxes.set_xlim([cur_xlim[0] + dx, cur_xlim[1] - dx])
-        event.inaxes.set_ylim([cur_ylim[0] + dy, cur_ylim[1] - dy])
+        zoomFactor = 1.2 
+        event.inaxes.set_xlim(cur_xlim[0] + dx * zoomFactor, cur_xlim[1] - dx * zoomFactor)
+        event.inaxes.set_ylim(cur_ylim[0] + dy * zoomFactor, cur_ylim[1] - dy * zoomFactor)
 
-    updateConnect()
+    updateConnections()
     event.inaxes.figure.canvas.draw()
+    event.inaxes.figure.canvas.flush_events()
     
 ##########################################################################################
 # python lineage.py testFile.csv 'Time (ka)' 'Stack Benthic d18O (per mil)' 'depthODP849cm' 'd18Oforams-b' pointers1.csv
@@ -315,25 +423,30 @@ fig, axs = plt.subplots(2, 1, figsize=(10,8), num='Lineage')
 
 #=========================================================================================
 curve1Color = 'red'
-curve1 = Line2D(x1, y1, color=curve1Color, picker=True, pickradius=20, linewidth=0.5, label='curve') 
-axs[0].add_artist(curve1)
+curve1, = axs[0].plot(x1, y1, color=curve1Color, picker=True, pickradius=20, linewidth=0.5, label='curve') 
 points1 = axs[0].scatter(x1, y1, s=5, marker='o', color=curve1Color, picker=True, pickradius=20, label='points')
 points1.set_visible(False)
 linecursor1 = axs[0].axvline(color='k', alpha=0.25, linewidth=1)
 axs[0].grid(visible=True, which='major', color='lightgray', linestyle='dashed', linewidth=0.5)
-axs[0].set_xlim(min(x1), max(x1))
-axs[0].set_ylim(min(y1), max(y1))
+axs[0].set_xlabel(x1Name)
+axs[0].set_ylabel(y1Name)
+axs[0].autoscale()
 
 #=========================================================================================
 curve2Color = 'forestgreen'
-curve2 = Line2D(x2, y2, color=curve2Color, picker=True, pickradius=20, linewidth=0.5, label='curve')
-axs[1].add_artist(curve2)
+curve2, = axs[1].plot(x2, y2, color=curve2Color, picker=True, pickradius=20, linewidth=0.5, label='curve')
 points2 = axs[1].scatter(x2, y2, s=5, marker='o', color=curve2Color, picker=True, pickradius=20, label='points')
 points2.set_visible(False)
 linecursor2 = axs[1].axvline(color='k', alpha=0.25, linewidth=1)
 axs[1].grid(visible=True, which='major', color='lightgray', linestyle='dashed', linewidth=0.5)
-axs[1].set_xlim(min(x2), max(x2))
-axs[1].set_ylim(min(y2), max(y2))
+axs[1].set_xlabel(x2Name)
+axs[1].set_ylabel(y2Name)
+axs[1].autoscale()
+
+#=========================================================================================
+axsInterp = axs[0].twinx()
+axsInterp.set_ylabel(y2Name)
+axsInterp.set_visible(False)
 
 #=========================================================================================
 fig.canvas.mpl_connect('key_press_event', onKeyPress)
@@ -346,6 +459,7 @@ fig.canvas.mpl_connect('scroll_event', zoom)
 
 #=========================================================================================
 readPointers(filePointers)
+drawConnections()
 
 #=========================================================================================
 plt.show()
